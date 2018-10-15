8d83945858c1e62445603421ecf2bbaa2b9ffe6f
Rick <39381070+Bickle100101@users.noreply.github.com>
2018-10-15 21:06:36 +0000

    Episode 1
    https://youtu.be/s4XqPdaPUAg
    
    Initial Commit.  GameObject Setup, UI Mockup, abnd some discussion about desired features

---
+++ Quill18Creates - Project Caprica/Assets/_Scripts/ColonyView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColonyView : MonoBehaviour {
+
+    //OnEnable Technically happens before Start()
+    //Called Everytime the ColonyView GameObject is Set to Enabled
+    //Including Possibly on Scene Load
+    private void OnEnable()
+    {
+        //Update data elements based on selected Colony
+    }
+
+    public object Colony;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+}
